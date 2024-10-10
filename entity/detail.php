784c4a8a1457a4d2c98878d57875dc2ee4f9bd49
Berzerker1031 <100453345+Berzerker1031@users.noreply.github.com>
2024-10-10 12:47:54 -0400

    Add files via upload

--- entity/detail.php
+++ entity/detail.php
@@ -43,9 +43,7 @@ function displayElement($element,$x) { ?>
                         
                         <li><a href="../index.php" class="nav-link px-2">Home</a></li>
                         <li><a href="index.php?x=new" class="nav-link px-2">Posts</a></li>
-                        <?php if(isset($_SESSION['email'])) echo'
-                        <li><a href="post.php" class="nav-link px-2">My Posts</a></li>
-                        <li><a href="create.php" class="nav-link px-2">Create New Post</a></li>'?>
+                        <li><a href="myPosts.php" class="nav-link px-2">My Posts</a></li>
                     </ul>
 
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
@@ -71,8 +69,8 @@ function displayElement($element,$x) { ?>
                     <?php } else { ?>
                        
                     <div class="text-end">
-                        <a class="btn btn-info me-2" href="sign-in.php" role="button">Login</a>
-                        <a class="btn btn-warning" href="sign-up.php" role="button">Sign Up</a>
+                        <a class="btn btn-info me-2" href="../sign-in.php" role="button">Login</a>
+                        <a class="btn btn-warning" href="../sign-up.php" role="button">Sign Up</a>
                     </div>
                         
                     <?php } ?>
@@ -80,10 +78,25 @@ function displayElement($element,$x) { ?>
             </div>
         </header>
 
-        <div class="border rounded bg-dark mx-5 jumbotron text-center text-white">
-            <h1><?php echo $blogdata[$post_id]['title'] ?></h1>
+        <div class="border rounded bg-dark mx-5 p-2 jumbotron text-white">
+            <div class="row">
+                <div class="col-6">
+                    <?php if(strlen($blogdata[$post_id]['picture'])>0) { ?>
+                        <img style="width:400px;height: 400px;" src="<?php echo $blogdata[$post_id]['picture']; ?>" class="rounded float-left" alt="...">
+                    <?php } else { ?>
+                        <img style="width:400px;height: 400px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22861%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20861%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_192771132f5%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A43pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_192771132f5%22%3E%3Crect%20width%3D%22861%22%20height%3D%22250%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22320.5124969482422%22%20y%3D%22144.2%22%3E861x250%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="rounded float-left" alt="...">
+                    <?php }?>
+                </div>
+                <div class="col text-center">
+                    <h1><?php echo $blogdata[$post_id]['title'] ?></h1>
+                    <p>Band: <?php echo $blogdata[$post_id]['band'] ?></p>
+                    <p>Album: <?php echo $blogdata[$post_id]['album'] ?></p>
+                    <p>Song: <?php echo $blogdata[$post_id]['song'] ?></p>
+                    <p>Tag(s): <?php echo $blogdata[$post_id]['tags'] ?></p>
+                </div>
+            </div>
             <hr>
-            <h3>
+            <h3 class="text-center">
                 <?php echo $blogdata[$post_id]['content'] ?>
             </h3>
             <h5>
@@ -96,8 +109,8 @@ function displayElement($element,$x) { ?>
                         }
                     }
                     fclose($fp);
+                    echo '<h6 class="fw-light text-center">'.$blogdata[$post_id]['author'].' | '.$blogdata[0]['time']['date'].'</h6>' 
                 ?>
-                <?php echo '<h6 class="fw-light">'.$blogdata[0]['author'].' | '.$blogdata[0]['time']['date'].'</h6>' ?>
             </h5>
         </div>
 
